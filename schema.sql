5a5218319d335789cff13cc7b3c9cae5b2ad0f6d
foofmeister <forest.summers@gmail.com>
2022-10-21 20:59:24 -0700

    edited schemas

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE `DB`;
 USE `DB`;
 
 
-DROP TABLE IF EXISTS `Dummy`;
+DROP TABLE IF EXISTS `ARTIST`;
 CREATE TABLE `Dummy` (
   `Dummy_column1` datetime,
   `Dummy_column2` varchar(100) NOT NULL
